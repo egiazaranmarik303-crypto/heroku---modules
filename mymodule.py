432cf872df4a56f987932db12bca454b343bcc47
egiazaranmarik303-crypto <egiazaranmarik303@gmail.com>
2026-09-22 16:04:04 +0400

    Delete mymodule.py

--- mymodule.py
+++
@@ -1,9 +0,0 @@
-from .. import loader, utils
-class MyModule(loader.Module):
-    strings = {
-        "name": "module"
-    }
-
-    @loader.command()
-    async def hello(self,message):
-        await utils.answer(message, "ПРИВЕТ!")
